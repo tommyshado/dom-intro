6c1e86f256d681bb4737ae13634353832e75659b
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-04-10 19:23:22 +0200

    added an event listener to the reset button and got reference to the radio buttons, I used .querySelectorAll where I selected all radio buttons

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,13 +1,17 @@
-// get a reference to the sms or call radio buttons
+// get reference to all radio buttons that share the same name
+const radioBillSettingsBtn = document.querySelectorAll('input[type="radio"][name="billItemTypeWithSettings"]');
+// get a reference to the add button
 const addCallAndSmsBtn = document.querySelector('.billTypeButton');
-
+// get the reference to the default button
+const defaultReset = document.querySelector('.defaultTypeButton');
+// get the refence to the callTotalSetting element
 const totalCallSetting = document.querySelector('.callTotalSettings');
-
+// get the refence to the smsTotalSetting element 
 const totalSmsSetting = document.querySelector('.smsTotalSettings');
 
 let callRadioBtnTotal = 0;
 let smsRadioBtnTotal = 0;
-
+// add an event listener to the button
 addCallAndSmsBtn.addEventListener('click', settingsAddFunc = () => {
     const radioBillType = document.querySelector(".billItemTypeWithSettings:checked");
 
@@ -19,4 +23,9 @@ addCallAndSmsBtn.addEventListener('click', settingsAddFunc = () => {
 
     totalCallSetting.innerHTML = callRadioBtnTotal.toFixed(2);
     totalSmsSetting.innerHTML = smsRadioBtnTotal.toFixed(2);
+})
+
+// add an event listener to the reset button
+defaultReset.addEventListener('click', settingsDefaultFunc = () => {
+
 })
