c212bb34875cde8a8d086b689c58ecb589652c2d
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-04-18 15:44:50 +0200

    created a factory functions on my settingBillFactory function and returning an object of functions

---
+++ js/tests/settings-bill-logic.js
@@ -0,0 +1,39 @@
+function settingsBillFactory() {
+    let smsCost = 0;
+    let callCost = 0;
+    let warningLevel = 0;
+    let criticalLevel = 0;
+
+    let setCallCost = function(callCostValue) {
+        callCost = callCostValue;
+    }
+
+    let getCallCost = function() {
+        return callCost;
+    }
+
+    let setSmsCost = function(smsCostValue) {
+        smsCost = smsCostValue;
+    }
+
+    let getSmsCost = function() {
+        return smsCost;
+    }
+
+    let setWarningLevel = function(warningLevelValue) {
+        warningLevel = warningLevelValue;
+    }
+
+    let setCriticalLevel = function(criticalLevelValue) {
+        criticalLevel = criticalLevelValue;
+    }
+
+    return {
+        setCallCost,
+        getCallCost,
+        setSmsCost,
+        getSmsCost,
+        setWarningLevel,
+        setCriticalLevel
+    }
+}
