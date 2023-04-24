15138eb2546321755b5c153b9c2c7f53ff3b15ac
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-04-24 15:14:28 +0200

    described get values, the function should be able to get the values in the form

---
+++ js/tests/text-bill-logic.test.js
@@ -0,0 +1,31 @@
+describe('textBillFactory', function() {
+    describe('get values', function() {
+        it('should be able to get values for the call cost', function() {
+            let returnedValue = textBillFactory();
+            returnedValue.callSmsCost('call');
+
+            assert.equal(returnedValue.getCallCost(), 2.75);
+
+            let returnedValue2 = textBillFactory();
+            returnedValue2.callSmsCost('call');
+
+            assert.equal(returnedValue2.getCallCost(), 2.75);
+        });
+
+        it('should be able to get values for the sms cost', function() {
+            let returnedValue = textBillFactory();
+            returnedValue.callSmsCost('sms');
+
+            assert.equal(returnedValue.getSmsCost(), 0.65);
+
+            let returnedValue2 = textBillFactory();
+            returnedValue2.callSmsCost('sms');
+
+            assert.equal(returnedValue2.getSmsCost(), 0.65);
+        });
+    })
+
+    describe('add the values to form a total', function() {
+        
+    })
+})
