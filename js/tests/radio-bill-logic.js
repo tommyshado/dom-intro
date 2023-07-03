b131300936701421c3a554fdafdab7e63eaa27de
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-07-03 22:24:40 +0200

    changed the function name from callSmsCost() to setSmsOrCallCost()

--- js/tests/radio-bill-logic.js
+++ js/tests/radio-bill-logic.js
@@ -2,7 +2,7 @@ const radioBillFactory = () => {
     let callTotal = 0;
     let smsTotal = 0;
 
-    const callSmsCost = (radioValue) => {
+    const setSmsOrCallCost = (radioValue) => {
         // in the DOM the button is checked
         if(radioValue === 'call') {
             callTotal += 2.75;
@@ -25,14 +25,14 @@ const radioBillFactory = () => {
 
     const addClassColor = () => {
         if (smsCallTotal() >= 50) {
-            return 'red';
+            return 'danger';
         } else if (smsCallTotal() >= 30) {
-            return 'orange';
+            return 'warning';
         }
     }
 
     return {
-        callSmsCost,
+        setSmsOrCallCost,
         getCallCost,
         getSmsCost,
         smsCallTotal,
